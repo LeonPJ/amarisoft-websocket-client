ca19b9b2b3dcfe2a9cc3e3fbc285c6629a79cf42
Leon Huang <04199@SERCOMM.COM>
2021-09-17 11:10:05 +0800

    add disconnect

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import { WebSocketServer } from 'ws';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const wss = new WebSocketServer({ port: 3000 });
+const server = express().listen(process.env.PORT, () => console.log(`URL ws://localhost:${process.env.PORT}`));
+const wss = new WebSocketServer({ server });
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
+        let received = JSON.parse(message)
+        console.log(`Client: ${received.message}`);
+        if (received.message == 'config_get')
+            ws.send('you request config_get');
     });
 
+    ws.on('close', function close() {
+        console.log('disconnected');
+    });
     //ws.send('something');
 });
